98c2157e8330c1d128b8e3095af2f9740603c129
PANxinkui131455 <154289178+PANxinkui131455@users.noreply.github.com>
2026-02-26 15:47:17 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   // 部署到 GitHub Pages 时，base 设为仓库名，例如: '/vape-pricing-dashboard/'
   // 如果使用自定义域名或根路径，改为 '/'
-  base: '/vape-pricing-dashboard/',
+  base: '/my-vape-dashboard/',
 })
